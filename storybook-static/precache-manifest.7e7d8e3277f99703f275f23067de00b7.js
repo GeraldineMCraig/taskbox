956a69c81ba4f354d49ffb89d9585ec529afc7e5
Geraldine Craig <gerriemcraig@outlook.com>
2020-10-03 04:20:23 -0700

    taskbox UI

---
+++ storybook-static/precache-manifest.7e7d8e3277f99703f275f23067de00b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d87dea531e88ab2773b9c838425c968",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bc4ae3ffbf05ef810187.bundle.js"
+  },
+  {
+    "url": "runtime~main.bc4ae3ffbf05ef810187.bundle.js"
+  },
+  {
+    "revision": "bc167c617721de67688e",
+    "url": "static/css/main.b15adadd.chunk.css"
+  },
+  {
+    "url": "vendors~main.bc4ae3ffbf05ef810187.bundle.js"
+  },
+  {
+    "url": "vendors~main.bc4ae3ffbf05ef810187.bundle.js.LICENSE.txt"
+  }
+]);
